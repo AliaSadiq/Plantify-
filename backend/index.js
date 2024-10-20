3b50df21499c506a84b796d3ae13740889f27389
Alia Sadiq <130149083+AliaSadiq@users.noreply.github.com>
2024-10-20 14:00:09 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -90,7 +90,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: ["https://plantify-frontend.vercel.app"],
+  origin: ["https://plantify-ui.vercel.app","https://plantifybackend.vercel.app"],
   methods: ["POST", "GET", "PUT", "DELETE"],
   // credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
